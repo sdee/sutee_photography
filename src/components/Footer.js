0ccbc4323419f273fa72b65b6c0f84c4a5f70d93
Sutee Dee <sutee.dee@invitae.com>
2019-03-21 20:19:33 -0700

    deletes footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,20 +12,6 @@ const Content = styled.footer`
 
 const Footer = () => (
   <Content>
-    &copy; 2018 by Gatsby Starter Portfolio - Emilia. Pictures by{' '}
-    <a href="https://unsplash.com/" target="_blank" rel="noopener noreferrer">
-      Unsplash
-    </a>
-    . Design by{' '}
-    <a href="https://www.lekoarts.de/en" target="_blank" rel="noopener noreferrer">
-      LekoArts
-    </a>
-    .<br />
-    Have a look at the{' '}
-    <a href="https://github.com/LekoArts/gatsby-starter-portfolio-emilia" target="_blank" rel="noopener noreferrer">
-      Github repository
-    </a>
-    !
   </Content>
 )
 
