19ba8123a864212fa82b05f5f1ebe7a63278a173
Sutee Dee <sutee.dee@invitae.com>
2019-04-28 12:25:45 -0700

    style tweaks

--- src/templates/placeGallery.js
+++ src/templates/placeGallery.js
@@ -16,7 +16,7 @@ const BG = styled.div`
 
 const Content = styled.footer`
   color: ${props => props.theme.colors.secondary};
-  text-align: left;
+  text-align: center;
   font-size: 0.9rem;
   padding-top: 3rem;
   padding-bottom: 3rem;
